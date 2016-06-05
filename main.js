6d1b3a13e58bfc6b61fe32fd2a6cbd95eb0fb7cf
y-sg <y.shiga.91+git@gmail.com>
2016-06-06 00:28:22 +0900

    セミコロン追加

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
 // ローカルストレージからデータ読み込み
 var task_data = [];
-if (window.localStorage.getItem('task_data') != null) {
+if (window.localStorage.getItem('task_data') !== null) {
   task_data = JSON.parse(window.localStorage.getItem('task_data'));
 }
-
 
 // データを表示する
 function displayTasks () {
   var ret = [];
   for (var i = task_data.length-1; i >= 0; i--) {
-    var line = '<tr>'
-    line += '<td class="col-xs-6 text-center">' + task_data[i]['taskName'] + '</td>'
+    var line = '<tr>';
+    line += '<td class="col-xs-6 text-center">' + task_data[i]['taskName'] + '</td>';
     line += '<td class="col-xs-2 text-center">' + task_data[i]['val'] + '/' + task_data[i]['maxVal'] + '</td>';
     line += '<td class="col-xs-4 text-center">';
-    line += '<div class="row form-group form-inline">'
-    line += '<div class="col-md-2"><button type="button" class ="btn btn-default" onClick="updateTask(' + String(i) + ', -10)">-10</button></div>'
-    line += '<div class="col-md-2"><button type="button" class ="btn btn-default" onClick="updateTask(' + String(i) + ', -1)"> - </button></div>'
-    line += '<div class="col-md-2"><button type="button" class ="btn btn-default" onClick="updateTask(' + String(i) + ',  1)"> + </button></div>'
-    line += '<div class="col-md-2"><button type="button" class ="btn btn-default" onClick="updateTask(' + String(i) + ', 10)">+10</button></div>'
-    line += '<div class="col-md-4"><button type="button" class ="btn btn-danger" onClick="deleteTask(' + String(i)+ ')">削除</button></div>'
-    line += '</div>'
+    line += '<div class="row form-group form-inline">';
+    line += '<div class="col-md-2"><button type="button" class ="btn btn-default" onClick="updateTask(' + String(i) + ', -10)">-10</button></div>';
+    line += '<div class="col-md-2"><button type="button" class ="btn btn-default" onClick="updateTask(' + String(i) + ', -1)"> - </button></div>';
+    line += '<div class="col-md-2"><button type="button" class ="btn btn-default" onClick="updateTask(' + String(i) + ',  1)"> + </button></div>';
+    line += '<div class="col-md-2"><button type="button" class ="btn btn-default" onClick="updateTask(' + String(i) + ', 10)">+10</button></div>';
+    line += '<div class="col-md-4"><button type="button" class ="btn btn-danger" onClick="deleteTask(' + String(i)+ ')">削除</button></div>';
+    line += '</div>';
     line += '</td>';
     line += '</tr>';
     ret.push(line);
     console.log(line);
   }
   $($('.task-list')).html(ret);
-}
+} 
 displayTasks();
 
 // ローカルストレージに保存
@@ -43,7 +42,7 @@ function addTask () {
   max_val = Number(max_val);
 
   //入力が数字でなければreturn
-  if (isNaN(max_val) || max_val == 0) {
+  if (isNaN(max_val) || max_val === 0) {
     alert('0以外の数字を入力！');
     return;
   }
